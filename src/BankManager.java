a0750536bd31421f07f50fb661fab93b5911a318
viakisun <kisun@viasofts.com>
2021-06-14 12:43:29 +0900

    update BankAccount

--- src/BankManager.java
+++ src/BankManager.java
@@ -36,6 +36,43 @@ public class BankManager {
 				continue;
 				
 			case BankCommand.CMD_DEPOSIT:
+				System.out.println(BankMessage.MSG_INPUT_ACCOUNT_ID);
+				
+				int accountID = 0;
+				try {
+					accountID = scanner.nextInt();				
+				} catch (InputMismatchException ex) {
+					System.out.println(ex.toString());
+					continue;
+				}
+				
+				boolean bFoundAccount = false;
+				
+				for(BankAccount bankAccount : bankAccounts) {
+					if (bankAccount.getAccountID() == accountID) {
+						bFoundAccount = true;
+						
+						System.out.println(BankMessage.MSG_SET_AMOUNT);
+						
+						int amount = 0;
+						try {
+							amount = scanner.nextInt();				
+						} catch (InputMismatchException ex) {
+							System.out.println(ex.toString());
+							continue;
+						}
+						
+						bankAccount.deposit(amount);
+						System.out.println(String.format(BankMessage.MSG_GET_ACCOUNT_ID, bankAccount.getAccountID()));
+						break;
+					}
+				}
+				
+				if (bFoundAccount == false) {
+					System.out.println(BankMessage.MSG_CANNOT_FIND_ACCOUNT);
+					continue;
+				}
+				
 				break;
 				
 			case BankCommand.CMD_EXIT:
--- src/BankMessage.java
+++ src/BankMessage.java
@@ -4,4 +4,7 @@ public class BankMessage {
 	public static final String MSG_SELECT_MENU = "> 업무를 선택해주세요\n 1)개설 2)입금 3)출금 4) 종료";
 	public static final String MSG_INPUT_NAME = "> 당신의 이름을 입력해주세요";
 	public static final String MSG_GET_ACCOUNT_ID = "> 계좌 번호는 %d입니다";
+	public static final String MSG_INPUT_ACCOUNT_ID = "> 계좌 번호를 입력하세요";
+	public static final String MSG_CANNOT_FIND_ACCOUNT = "> 계좌를 찾을 수 없습니다";
+	public static final String MSG_SET_AMOUNT = "> 입금 금액을 넣으세요";
 }
